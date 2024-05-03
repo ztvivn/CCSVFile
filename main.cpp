a1393060177905c244dc69b77585577aebfd1f96
ztvivn <113670962+ztvivn@users.noreply.github.com>
2024-05-03 10:03:37 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 class CFile
 {
     public:
-    virtual void Seek(const std::string value) = 0;
-    virtual std::string Read(int line_number) = 0;
-    virtual void Write(std::string value) = 0;
+    void Seek(const std::string value) {};
+    std::string Read(int line_number) {return "";};
+    void Write(std::string value) {};
     // virtual void GetPosition() = 0;
     // virtual void GetLength() = 0;
 };
@@ -123,9 +123,58 @@ void CCSVFile::Write(std::string value)
     file << value << std::endl;
 }
 
-int main()
-{
-    CCSVFile file("pr");
+class CBinFile {
+private:
+    std::fstream file;
+    std::string nameFile;
+    const std::string format = ".bin";
+
+    struct People {
+        int index;
+        int age;
+        int height;
+    };
+
+public:
+    CBinFile(std::string nameFile) : nameFile(nameFile + ".bin") {
+        file.open(this->nameFile, std::ios::binary | std::ios::in | std::ios::out | std::ios::app);
+        if (!file.is_open()) {
+            std::cerr << "Не удалось открыть файл!\n";
+        }
+    }
+
+    ~CBinFile() {
+        if (file.is_open())
+            file.close();
+    }
+
+    void Write(int name, int age, int height) {
+        People people;
+        people.index = name;
+        people.age = age;
+        people.height = height;
+
+        file.write(reinterpret_cast<char*>(&people), sizeof(People));
+    }
+
+    void Read(int number) {
+        People people;
+        file.seekg(number * sizeof(People), std::ios::beg);
+        file.read(reinterpret_cast<char*>(&people), sizeof(People));
+
+        if (!file.eof()) {
+            std::cout << "Структура №" << number << '\n';
+            std::cout << "name = " << people.index << '\n';
+            std::cout << "age = " << people.age << '\n';
+            std::cout << "height = " << people.height << '\n';
+        } else {
+            std::cerr << "Достигнут конец файла.\n";
+        }
+    }
+};
+
+int main() {
+    CCSVFile file("info");
 
     file.Seek("Ivan");
     std::cout << file.Read(4) << "\n";
@@ -133,6 +182,10 @@ int main()
     file.Write("7, Anton, 26, 55");
     std::cout << file.Read(6) << "\n";
 
+    CBinFile file1("Br");
+    file1.Write(3, 22, 179);
+
+    file1.Read(0);
 
     return 0;
 }
